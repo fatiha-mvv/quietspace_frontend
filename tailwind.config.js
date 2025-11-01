5f827a0c44e241b5058f3aa1707180657cabc2a3
124fatiha <103788376+124fatiha@users.noreply.github.com>
2025-11-01 21:35:00 +0100

    Initial commit - frontend QuietSpace

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      colors: {
+        primary: '#4A6CF7',
+        secondary: '#13C296',
+        dark: '#1D2144',
+        'body-color': '#637381',
+        warning: '#E4A11B',
+        danger: '#DC4C64',
+        success: '#13C296',
+        info: '#3C91E6',
+      },
+      boxShadow: {
+        signUp: '0px 5px 10px rgba(4, 10, 34, 0.2)',
+        one: '0px 2px 3px rgba(7, 7, 77, 0.05)',
+        sticky: 'inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+}
